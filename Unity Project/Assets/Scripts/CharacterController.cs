fdd2e775abec824f46d8df24e704a6cbb0a3e0d8
Marshall Sharp <marshalljacob.sharp@gmail.com>
2023-04-11 19:32:37 +0100

    Character Prefab added, Will recode doors

--- Unity Project/Assets/Scripts/CharacterController.cs
+++ Unity Project/Assets/Scripts/CharacterController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Door : MonoBehaviour
+public class CharacterController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,7 +13,6 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
     }
-
 }
-       
