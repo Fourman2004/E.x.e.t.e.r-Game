bf4fc6a2a36f57e13a4ec7ec84ac0ef5f5a9c216
Marshall Sharp <marshalljacob.sharp@gmail.com>
2023-04-12 21:23:06 +0100

    Door works

--- Unity Project/Assets/Scripts/Door.cs
+++ Unity Project/Assets/Scripts/Door.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+
+    public Collider Doorcollider;
+    public GameObject Dooraxis;
+    public bool doopen;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +16,29 @@ public class Door : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
+        if(!doopen && Doorcollider)
+        {
+            OpenDoor();
+            doopen = false;
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+
+
+    public void OpenDoor()
+    {
+     Dooraxis.transform.Translate(new Vector3(10.0f, 0.0f, 0.0f));
+    }
+
+    public void CloseDoor()
+    {
+        Dooraxis.transform.Translate(new Vector3(-10.0f,0.0f,0.0f));
     }
 
 }
