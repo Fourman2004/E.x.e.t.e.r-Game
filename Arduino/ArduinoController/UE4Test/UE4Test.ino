5f3141b0784df5e5486c3feee2fada746b346958
Marshall Sharp <ms279228@falmouth.ac.uk>
2023-04-24 16:11:05 +0100

    Buttons being recoded

--- Arduino/ArduinoController/UE4Test/UE4Test.ino
+++ Arduino/ArduinoController/UE4Test/UE4Test.ino
@@ -6,53 +6,33 @@ float YaxisValue = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  /**pinMode(3, INPUT_PULLUP);
-  pinMode(9, OUTPUT);**/
-  pinMode(10, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(9, OUTPUT);
+  pinMode(6, INPUT_PULLUP);
   pinMode(11, OUTPUT);
   pinMode(5, INPUT_PULLUP);
-  pinMode(6, OUTPUT);
-  pinMode(13, INPUT_PULLUP);
+  pinMode(10, OUTPUT);
   pinMode(12, OUTPUT);
   pinMode(4, INPUT_PULLUP);
-  pinMode(7, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  InteractButton();
-  /**Yaxis();
-  Xaxis();
-  Movement();**/
+  
+ //Up();
+ left();
 }
 
-/**void Yaxis() {
-YaxisValue = map(analogRead(Yaxislog), 0, 1023,0 ,1023);
-Serial.println("Y Axis:" + analogRead(Yaxislog));
-}
-
-void Xaxis() {
-  XaxisValue = map(analogRead(Xaxislog), 0, 1023, 0, 1023);
-  Serial.println("X Axis:" + analogRead(Xaxislog));
-}**/
-
-void InteractButton()
+void Up()
 {
   digitalWrite(9,!digitalRead(3));
   Serial.print(!digitalRead(3));
   Serial.println();
-}/**
+}
 
-void Movement()
+void left()
 {
-  digitalWrite(6,!digitalRead(5));
-  digitalWrite(12,!digitalRead(13));
-  digitalWrite(7, digitalRead(4));
-  digitalWrite(11,!digitalRead(10));
- Serial.print(!digitalRead(5));
- Serial.print(!digitalRead(13));
- Serial.print(digitalRead(4));
- Serial.print(!digitalRead(10));
- delay(1000); 
- Serial.println();
-}**/
+digitalWrite(10 ,!digitalRead(5));
+Serial.print(!digitalRead(5));
+Serial.println();
+}
